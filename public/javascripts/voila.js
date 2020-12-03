02b7366374a7e84930122692fe4fb3c4ba667995
Michael Akinlosotu <miketechonetwo@Michaels-MBP.fios-router.home>
2020-12-03 09:52:18 -0500

    port again

--- public/javascripts/voila.js
+++ public/javascripts/voila.js
@@ -7,66 +7,66 @@ $("#submitButton").click(function() {
 
 
 
-  picCount ++;
-  var newCount = picCount;
-  var prompt = "";
-
- console.log("pic count = " + picCount);
- console.log("new count = " + newCount);
-  switch (picCount) {
-
-    case 1 :
-
-     prompt = "<h2>  Upload Front Facing After Picture </h2>"
-
-    break;
-
-    case 2 :
-
-    prompt = "<h2 >  Upload Left Facing Before Picture   </h2>"
-
-    break;
-
-    case 3 :
-
-    prompt = "<h2> Upload Left Facing After Picture    </h2>"
-
-    break;
-
-    case 4 :
-
-    prompt = "<h2>  Upload Right Facing Before Picture   </h2>"
-
-    break;
-
-    case 5 :
-
-    prompt = "<h2> Upload Right Facing After Picture  </h2>"
-
-    break;
-
-    case 6 :
-
-    prompt = "<h2> Upload Back Facing Before Picture    </h2>"
-
-    break;
-
-    case 7 :
-
-    prompt = "<h2>  Upload Back Facing After Picture   </h2>"
-    break;
-
-  }
-
-  if (picCount === 8) {
-  $("#cloudUpload").submit();
-}
-else {
+//   picCount ++;
+//   var newCount = picCount;
+//   var prompt = "";
+//
+//  console.log("pic count = " + picCount);
+//  console.log("new count = " + newCount);
+//   switch (picCount) {
+//
+//     case 1 :
+//
+//      prompt = "<h2>  Upload Front Facing After Picture </h2>"
+//
+//     break;
+//
+//     case 2 :
+//
+//     prompt = "<h2 >  Upload Left Facing Before Picture   </h2>"
+//
+//     break;
+//
+//     case 3 :
+//
+//     prompt = "<h2> Upload Left Facing After Picture    </h2>"
+//
+//     break;
+//
+//     case 4 :
+//
+//     prompt = "<h2>  Upload Right Facing Before Picture   </h2>"
+//
+//     break;
+//
+//     case 5 :
+//
+//     prompt = "<h2> Upload Right Facing After Picture  </h2>"
+//
+//     break;
+//
+//     case 6 :
+//
+//     prompt = "<h2> Upload Back Facing Before Picture    </h2>"
+//
+//     break;
+//
+//     case 7 :
+//
+//     prompt = "<h2>  Upload Back Facing After Picture   </h2>"
+//     break;
+//
+//   }
+//
+//   if (picCount === 8) {
+//   $("#cloudUpload").submit();
+// }
+// else {
 
 $(".upload-prompt").html(prompt);
 
 }
-});
+// });
 
 $(".front").one("click",function() {
   $(".after-front").fadeIn(400);
